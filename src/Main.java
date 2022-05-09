a0ec90d035dc1a6ff615ac708894a156016da37d
dominikmoskal <100564913+dominikmoskal@users.noreply.github.com>
2022-05-09 14:21:43 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System . się . println ( "Witaj świecie!" );
-        System . się . drukujln ( „Moje  zmiany” );
-    }
+        System . się . drukujln ( „Moje      zmiany” );
+    } 
 }
